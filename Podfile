ba82e7260814c781e3b7f26d3d0308987e5269a5
kaanemec <kaanemec@gmail.com>
2016-08-19 22:59:06 +0300

    Added external libraries: Alamofire & SwiftyJSON

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SpotifySearch' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  
+  pod 'SwiftyJSON', '~> 2.3'
+  pod 'Alamofire', '~> 3.4'
+  
+  # Pods for SpotifySearch
+
+end
--- SpotifySearch/ViewController.swift
+++ SpotifySearch/ViewController.swift
@@ -9,7 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let minSearchInterval: NSTimeInterval = NSTimeInterval(1.0)
+    var webSearchStartTime: NSDate!
+    var refreshSearchTimer: NSTimer!
 
+    var suggestions: [spotifySearchResult]!
+    var SpotifySearch = spotifySearch()
+    
+    @IBOutlet weak var resultsField: UITextView!
+    @IBOutlet weak var searchField: UITextField!
+    
+    
+    @IBAction func textEditing(sender: UITextField, forEvent event: UIEvent) {
+        
+        if searchField.text?.characters.count>2 {
+            print(searchField.text)
+            if SpotifySearch.searching {
+                if NSDate().timeIntervalSinceDate(webSearchStartTime) > minSearchInterval {
+                    if refreshSearchTimer != nil { refreshSearchTimer.invalidate() }
+                    SpotifySearch.stopSearching()
+                    SpotifySearch.searchSpotifyFor(sender.text!, searchType: spotifySearchType.Song, handler: self.handleSongResults)
+                    webSearchStartTime = NSDate()
+                } else {
+                    if refreshSearchTimer != nil { refreshSearchTimer.invalidate() }
+                    refreshSearchTimer = NSTimer.scheduledTimerWithTimeInterval(minSearchInterval - NSDate().timeIntervalSinceDate(webSearchStartTime), target: self, selector: #selector(self.refreshSongSearch), userInfo: nil, repeats: false)
+                }
+            } else {
+                if refreshSearchTimer != nil { refreshSearchTimer.invalidate() }
+                SpotifySearch.searchSpotifyFor(self.searchField.text!, searchType: spotifySearchType.Song, handler: self.handleSongResults)
+                webSearchStartTime = NSDate()
+            }
+        } else {
+            if SpotifySearch.searching { SpotifySearch.stopSearching() }
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +56,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func handleSongResults(results: [spotifySearchResult]) {
+        if results.count > 0 {
+            resultsField.text = ""
+            if SpotifySearch.searching { SpotifySearch.stopSearching() }
+            if refreshSearchTimer != nil { refreshSearchTimer.invalidate() }
+            
+            for eachResult in results {
+                resultsField.text = resultsField.text + eachResult.artist + " - " + eachResult.track + "\n\n"
+            }
+        }
+    }
+    
+    
+    func refreshSongSearch() {
+        if SpotifySearch.searching { SpotifySearch.stopSearching() }
+        SpotifySearch.searchSpotifyFor(searchField.text!, searchType: spotifySearchType.Song, handler: self.handleSongResults)
+    }
 
 }
 
